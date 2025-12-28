c9f1cc12ebc2d4867940d2ab6a26e53c7f4f4184
jinan <awadaa468@gmail.com>
2025-12-29 01:50:11 +0200

    Update server.js to use env vars for MySQL

--- server/server.js
+++ server/server.js
@@ -31,12 +31,14 @@ const upload = multer({ storage });
 /* -------------------- INIT + DB -------------------- */
 async function init() {
   // ✅ create promise connection
-  const db = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "KindnessWall",
-  });
+const db = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+});
+
 
   console.log("✅ Connected to MySQL database");
 
